c5c3ad9e3caf3945e0bee94725c14bf20b2dc548
chillin9panda <duremarkastu@gmail.com>
2024-10-09 17:28:43 +0300

    for loops done on java

--- forLoops.java
+++ forLoops.java
@@ -21,8 +21,38 @@ class forLoops {
 		}
 	}
 
+	public static void evenNumbs() {
+		for (int i = 0; i <= 50; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+
+		}
+	}
+
+	public static void factorial(int num) {
+		int total = 1;
+
+		for (int i = num; i >= 1; i--) {
+			total *= i;
+		}
+		System.out.println(total);
+	}
+
+	public static void largestNum() {
+		int[] nums = { 5, 12, 1, 22, 15, 6 };
+		int smallest = 999999;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] < smallest) {
+				smallest = nums[i];
+			}
+		}
+		System.out.println(smallest);
+	}
+
 	public static void main(String[] args) {
-		multTable(5);
+		largestNum();
 
 	}
 }
