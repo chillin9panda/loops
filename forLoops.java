1b32f0103535af183260948cb3cb3fa0a04d875d
chillin9panda <duremarkastu@gmail.com>
2024-10-09 04:45:34 +0300

    for loops

--- forLoops.java
+++ forLoops.java
@@ -1,5 +1,28 @@
 class forLoops {
+	public static void nums() {
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void sumOfN(int n) {
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println("Sum: " + sum);
+	}
+
+	public static void multTable(int n) {
+		int answ;
+		for (int i = 0; i <= 10; i++) {
+			answ = n * i;
+			System.out.println(n + " * " + i + " = " + answ);
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("For Loops Review.");
+		multTable(5);
+
 	}
 }
